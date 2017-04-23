d6bd29c675ad27a9af1f3cd62b3d722b149c754e
Harald Held <harald.held@gmail.com>
2017-04-23 20:45:35 +0200

    Added code to decode base64 encoded file contents.
    Also switched to return type std::string to avoid memory leaks.

--- cppGeneration.go
+++ cppGeneration.go
@@ -10,9 +10,11 @@ import (
 const headerTemplate = `#ifndef RESPPGEN_H
 #define RESPP_H
 
+#include <string>
+
 namespace ResPP
 {
-const char* R(const char* prefix, const char* path);
+std::string R(const char* prefix, const char* path);
 }
 
 #endif // RESPP_H
@@ -20,7 +22,7 @@ const char* R(const char* prefix, const char* path);
 
 const sourceTemplate = `#include "ResPP.h"
 #include <map>
-#include <string>
+#include <string.h>
 {{$len := len .Contents}}
 namespace
 {
@@ -38,11 +40,77 @@ namespace
 		{"{{.Prefix}}", &{{.Prefix | prefixToMapName}}},
 		{{- end}}
 	};
+
+	const char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                                "abcdefghijklmnopqrstuvwxyz"
+                                "0123456789+/=";
+
+        long indexIn(const char *a, char c)
+	{
+	    const char *p = strchr(a, c);
+
+	    if(p)
+	    {
+		return p - a;
+	    }
+
+	    return 0;
+	}
+
+	char *b64_decode(const char *s, const unsigned long numBytes, unsigned long *decodedNumBytes)
+	{
+	    if(!numBytes%4) return 0;
+
+	    // This can only be 0, 1, or 2. Otherwise, something is wrong ...
+	    size_t numPaddingChars = 0;
+
+	    if(numBytes>=2)
+	    {
+		if(s[numBytes-1]=='=') ++numPaddingChars;
+		if(s[numBytes-2]=='=') ++numPaddingChars;
+	    }
+
+	    const size_t out_len = 3*numBytes/4-numPaddingChars;
+
+	    char *out = (char*)malloc(out_len+1);
+	    out[out_len] = '\0';
+
+	    int j = 0;
+	    long b[4];
+	    size_t i;
+
+	    for(i=0; i<numBytes; i+=4)
+	    {
+		b[0] = indexIn(alphabet, s[i]);
+		b[1] = indexIn(alphabet, s[i+1]);
+		b[2] = indexIn(alphabet, s[i+2]);
+		b[3] = indexIn(alphabet, s[i+3]);
+
+		out[j++] = (char) ((b[0] << 2) | (b[1] >> 4));
+
+		if(b[2] < 64)
+		{
+		    out[j++] = (char) ((b[1] << 4) | (b[2] >> 2));
+
+		    if(b[3] < 64)
+		    {
+			out[j++] = (char) ((b[2] << 6) | b[3]);
+		    }
+		}
+	    }
+
+	    if(decodedNumBytes)
+	    {
+		*decodedNumBytes = out_len;
+	    }
+
+	    return out;
+	}
 }
 
 namespace ResPP
 {
-const char* R(const char* prefix, const char* path)
+std::string R(const char* prefix, const char* path)
 {
 	auto prefix_it = prefixToIdx.find(prefix);
 
@@ -55,11 +123,25 @@ const char* R(const char* prefix, const char* path)
 		if(path_it!=m->cend())
 		{
 			auto encodedContent = path_it->second;
-			return encodedContent;
+
+			if(encodedContent)
+			{
+				std::string s(encodedContent);
+				unsigned long decodedNumBytes;
+
+				auto dec = b64_decode(s.c_str(), s.size(), &decodedNumBytes);
+				auto decStr = std::string(dec);
+
+				free(dec);
+
+				return decStr;
+			}
+
+			return std::string(encodedContent);
 		}
 	}
 
-	return nullptr;
+	return std::string();
 }
 }
 `
